59e0b9cc8c0fa76e894bba86ddf080127b9131e3
gauravgarwa <gauravgarwa033@gmail.com>
2023-05-17 19:06:45 +0530

    up-18may

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "59188c04924b5ccc"; }
+/******/ 		__webpack_require__.h = function() { return "4f97e9bdd5722e67"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/navbar.js
+++ components/navbar.js
@@ -61,7 +61,7 @@ const Navbar = props => {
       position="fixed"
       as="nav"
       w="100%"
-      bg={useColorModeValue('#00de8d', '#00')}
+      bg={useColorModeValue('#ffff', '#0000')}
       css={{ backdropFilter: 'blur(10px)' }}
       zIndex={2}
       {...props}
