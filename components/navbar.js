64c7f12e60455184916586c900707771b0e67bc4
Gaurav Garwa <78600377+gaurav1832@users.noreply.github.com>
2024-08-09 11:57:22 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,158 @@
-import { forwardRef } from "react";
-import Logo from "./logo";
-import NextLink from "next/link";
+// import { forwardRef } from "react";
+// import Logo from "./logo";
+// import NextLink from "next/link";
 
+// import {
+//   Container,
+//   Box,
+//   Link,
+//   Stack,
+//   Heading,
+//   Flex,
+//   Menu,
+//   MenuItem,
+//   MenuList,
+//   MenuButton,
+//   IconButton,
+//   useColorModeValue,
+//   Center,
+//   Code,
+//   Divider,
+//   Button,
+// } from "@chakra-ui/react";
+// import { HamburgerIcon } from "@chakra-ui/icons";
+// import ThemeToggleButton from "./theme-toggle-button";
+// import Paragraph from "../components/paragraph";
+
+// import {
+//   IoLogoTwitter,
+//   IoLogoInstagram,
+//   IoLogoGithub,
+//   IoLogoLinkedin,
+//   IoMail,
+// } from "react-icons/io5";
+
+// const LinkItem = ({ href, path, target, children, ...props }) => {
+//   const active = path === href;
+//   const inactiveColor = useColorModeValue("gray.800", "whiteAlpha.900");
+//   return (
+//     <Link
+//       as={NextLink}
+//       href={href}
+//       scroll={false}
+//       p={2}
+//       bg={active ? "transparent" : undefined}
+//       color={active ? "teal" : inactiveColor}
+//       target={target}
+//       {...props}
+//     >
+//       {children}
+//     </Link>
+//   );
+// };
+
+// const MenuLink = forwardRef((props, ref) => (
+//   <Link ref={ref} as={NextLink} {...props} />
+// ));
+
+// const Navbar = (props) => {
+//   const { path } = props;
+
+//   return (
+//     <Box
+//       position="fixed"
+//       as="nav"
+//       w="100%"
+//       bg={useColorModeValue("transparent", "#0000")}
+//       css={{ backdropFilter: "blur(10px)" }}
+//       zIndex={2}
+//       {...props}
+//     >
+//       <Container
+//         display="flex"
+//         flexDirection={"row"}
+//         p={2}
+//         width={"100%"}
+//         wrap="wrap"
+//         gap={10}
+//       >
+//         <LinkItem href="./" path={path}>
+//           <Code
+//             variant={"outline"}
+//             fontWeight={900}
+//             fontFamily={""}
+//             fontSize={"18px"}
+//             padding={2}
+//             colorScheme="cyan"
+//             children=" <GauravGarwa/>"
+//           />
+//         </LinkItem>
+//         <Stack
+//           direction={{ base: "column", md: "row" }}
+//           display={{ base: "none", md: "flex" }}
+//           width={{ base: "full", md: "auto" }}
+//           alignItems="center"
+//           flexGrow={1}
+//           mt={{ base: 1, md: 0 }}
+//           padding={2}
+//           gap={10}
+//         >
+//           <LinkItem href="/about" path={path}>
+//             <Code colorScheme="pink" size={40} children="About" />
+//           </LinkItem>
+//           <LinkItem href="/projects" path={path}>
+//             <Code colorScheme="green" size={40} children="Projects" />
+//           </LinkItem>
+
+//           <Link
+//             href="https://drive.google.com/file/d/1-4WGHl1LUioDFAP4Nq1eR77Z0jLFadQO/view?usp=sharing"
+//             target="_blank"
+//           >
+//             <Button variant="ghost" color="#178edd" backgroundColor={"#EDF2F7"}>
+//               Resume
+//             </Button>
+//           </Link>
+
+//           {/* <ThemeToggleButton /> */}
+//         </Stack>
+
+//         <Box flex={1} align="right">
+//           <Box ml={2} display={{ base: "inline-block", md: "none" }}>
+//             <Menu isLazy id="navbar-menu">
+//               <MenuButton
+//                 as={IconButton}
+//                 icon={<HamburgerIcon />}
+//                 variant="outline"
+//                 aria-label="Options"
+//               />
+//               <MenuList>
+//                 <MenuItem as={MenuLink} href="/about">
+//                   About
+//                 </MenuItem>
+//                 <MenuItem as={MenuLink} href="/projects">
+//                   Projects
+//                 </MenuItem>
+//                 <MenuItem as={MenuLink} href="/certifications">
+//                   Certifications
+//                 </MenuItem>
+//                 {/* <MenuItem as={MenuLink} href="/blogs">
+//                   Blogs
+//                 </MenuItem> */}
+//               </MenuList>
+//             </Menu>
+//           </Box>
+//         </Box>
+//       </Container>
+//       <Divider />
+//     </Box>
+//   );
+// };
+
+// export default Navbar;
+
+import { forwardRef } from "react";
+import NextLink from "next/link";
+import Image from "next/image";
 import {
   Container,
   Box,
@@ -17,12 +168,9 @@ import {
   useColorModeValue,
   Center,
   Code,
-  Divider,
   Button,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import ThemeToggleButton from "./theme-toggle-button";
-import Paragraph from "../components/paragraph";
 
 import {
   IoLogoTwitter,
@@ -32,8 +180,10 @@ import {
   IoMail,
 } from "react-icons/io5";
 
+import nameImage from "../public/images/name.png";
+
 const LinkItem = ({ href, path, target, children, ...props }) => {
-  const active = path === href;
+  let active = path === href;
   const inactiveColor = useColorModeValue("gray.800", "whiteAlpha.900");
   return (
     <Link
@@ -42,7 +192,7 @@ const LinkItem = ({ href, path, target, children, ...props }) => {
       scroll={false}
       p={2}
       bg={active ? "transparent" : undefined}
-      color={active ? "teal" : inactiveColor}
+      color={active ? "yellow.300" : inactiveColor}
       target={target}
       {...props}
     >
@@ -62,31 +212,16 @@ const Navbar = (props) => {
     <Box
       position="fixed"
       as="nav"
-      w="100%"
-      bg={useColorModeValue("transparent", "#0000")}
+      className="w-fit"
       css={{ backdropFilter: "blur(10px)" }}
       zIndex={2}
       {...props}
     >
-      <Container
-        display="flex"
-        flexDirection={"row"}
-        p={2}
-        width={"100%"}
-        wrap="wrap"
-        gap={10}
-      >
-        <LinkItem href="./" path={path}>
-          <Code
-            variant={"outline"}
-            fontWeight={900}
-            fontFamily={""}
-            fontSize={"18px"}
-            padding={2}
-            colorScheme="cyan"
-            children=" <GauravGarwa/>"
-          />
+      <Container className="flex justify-center items-center" p={2} gap={10}>
+        <LinkItem href="/" path={path}>
+          <Image src={nameImage} width={110} height={70}></Image>
         </LinkItem>
+
         <Stack
           direction={{ base: "column", md: "row" }}
           display={{ base: "none", md: "flex" }}
@@ -98,26 +233,26 @@ const Navbar = (props) => {
           gap={10}
         >
           <LinkItem href="/about" path={path}>
-            <Code colorScheme="pink" size={40} children="About" />
+            <h2 className="font-bold">About</h2>
           </LinkItem>
           <LinkItem href="/projects" path={path}>
-            <Code colorScheme="green" size={40} children="Projects" />
+            <h2 className="font-bold">Portfolio</h2>
           </LinkItem>
 
-          <Link
+          <LinkItem
             href="https://drive.google.com/file/d/1-4WGHl1LUioDFAP4Nq1eR77Z0jLFadQO/view?usp=sharing"
             target="_blank"
           >
-            <Button variant="ghost" color="#178edd" backgroundColor={"#EDF2F7"}>
-              Resume
-            </Button>
-          </Link>
-
-          {/* <ThemeToggleButton /> */}
+            <h2 className="font-bold">Resume</h2>
+          </LinkItem>
         </Stack>
 
         <Box flex={1} align="right">
-          <Box ml={2} display={{ base: "inline-block", md: "none" }}>
+          <Box
+            ml={2}
+            display={{ base: "inline-block", md: "none" }}
+            className="!bg-black"
+          >
             <Menu isLazy id="navbar-menu">
               <MenuButton
                 as={IconButton}
@@ -125,14 +260,18 @@ const Navbar = (props) => {
                 variant="outline"
                 aria-label="Options"
               />
-              <MenuList>
-                <MenuItem as={MenuLink} href="/about">
+              <MenuList className="!bg-black">
+                <MenuItem as={MenuLink} href="/about" className="!bg-black">
                   About
                 </MenuItem>
-                <MenuItem as={MenuLink} href="/projects">
+                <MenuItem as={MenuLink} href="/projects" className="!bg-black">
                   Projects
                 </MenuItem>
-                <MenuItem as={MenuLink} href="/certifications">
+                <MenuItem
+                  as={MenuLink}
+                  href="/certifications"
+                  className="!bg-black"
+                >
                   Certifications
                 </MenuItem>
                 {/* <MenuItem as={MenuLink} href="/blogs">
@@ -143,7 +282,6 @@ const Navbar = (props) => {
           </Box>
         </Box>
       </Container>
-      <Divider />
     </Box>
   );
 };
